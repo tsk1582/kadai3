f0f52a85dd53c7ae601aeef88c96466969d3386c
tsk15 <tsk1582@gmail.com>
2023-08-04 19:03:02 +0900

    first version

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> stations = new HashMap<String, Integer>();
+
+        stations.put("上野駅", 60);
+        stations.put("東京駅", 70);
+        stations.put("北千住駅", 0);
+
+        for (String key : stations.keySet()) {
+            int value = stations.get(key);
+            System.out.println(key + "まで" + value + "分かかります");
+
+            if (value < 1) {
+                throw new IllegalArgumentException("所要時間は0分以上を指定してください。指定地=" + value);
+            }
+
+
+        }
+    }
+}
+
+
+
